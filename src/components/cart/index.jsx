9124a3937c1ee15191e8731957543c032c4bd935
Italo Marcelo <devfrontend2028study@gmail.com>
2025-04-04 10:26:31 -0300

    up

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -21,7 +21,7 @@ export default function Cart() {
 
   return (
     <div>
-         <Badge badgeContent={show ? cart : 0} onClick={OpenMenu} color='hsl(26, 100%, 55%)'>
+         <Badge badgeContent={show ? cart : 0} onClick={OpenMenu} color='warning' >
         <Stack width={{xs: '25px', lg: '20px'}}>
         <img src={CartIcon} alt='cart icon' />
         </Stack>
@@ -32,7 +32,7 @@ export default function Cart() {
             <hr/>
             {
               cart > 0 ?    <Stack id='container' display='flex' flexDirection='column'>
-              <Stack display='flex' flexDirection='row' marginTop='1em' padding='1em' justifyContent='space-between' alignContent='center'>
+              <Stack display='flex' flexDirection='row' marginTop={{ lg: '1em'}} padding='1em' justifyContent='space-between' alignContent='center'>
                 <Stack id='block-miniatura' marginRight='1em'>
                    <img src={Mini} style={{width: '40px', borderRadius: '4px'}} alt='' />
                 </Stack>
